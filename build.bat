2e46b163ff457904b68f07801be01cee621d580f
Aaron Erhart <ace7270@rit.edu>
2021-02-13 23:57:22 -0500

    fixed java cmds

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 javac -d src/main/java/build -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar src/main/java/\*.java
-java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore src/test/java/EdgeConnectorTest
+java src/main/java/RunEdgeConvert
